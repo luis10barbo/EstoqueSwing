f9bed9ddc358b509fe3953b747acc911faa963e9
kassio-23-dev <132306006+kassio-23-dev@users.noreply.github.com>
2023-10-29 18:32:46 -0300

    Classe  Ordem -  Projeto EstoqueSwing
    Criação das classes e definição dos atributos.

---
+++ Ordem_Compra.java
@@ -0,0 +1,15 @@
+package Modelo_ordem;
+
+public class Ordem_Compra extends Ordem{
+
+
+public Ordem_Compra(String responsavel, double valor, int quntidadeProduto) {
+super(responsavel, valor, quntidadeProduto);
+		
+	}
+
+	
+	
+	
+	
+}
