56f74fb630d6922e57ce639398fdb8d31019c90c
luis10barbo <luis10barbo2020@gmail.com>
2023-11-04 14:45:09 -0300

    feat: Adicionado modelo Estoque

---
+++ src/main/java/estoqueswing/model/Estoque.java
@@ -0,0 +1,30 @@
+package estoqueswing.model;
+
+public class Estoque {
+    String nome;
+    String descricao;
+
+    public Estoque() {}
+
+    public Estoque(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
