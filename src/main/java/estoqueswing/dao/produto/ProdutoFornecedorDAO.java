458d34f9a26b9c8ab210ebb24a821003eeea989a
luis10barbo <luis10barbo2020@gmail.com>
2023-11-04 15:49:39 -0300

    feat: Criado classe ProdutoFornecedor

--- src/main/java/estoqueswing/dao/produto/ProdutoFornecedorDAO.java
+++ src/main/java/estoqueswing/dao/produto/ProdutoFornecedorDAO.java
@@ -2,7 +2,7 @@ package estoqueswing.dao.produto;
 
 public class ProdutoFornecedorDAO {
     public static final String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS produtosFornecedor (" +
-            "idProdutoFornecedora INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "idProdutoFornecedor INTEGER PRIMARY KEY AUTOINCREMENT," +
             "idFornecedor INTEGER," +
             "idProduto INTEGER," +
             "valorProduto REAL," +
---
+++ src/main/java/estoqueswing/model/produto/ProdutoFornecedor.java
@@ -0,0 +1,53 @@
+package estoqueswing.model.produto;
+
+import estoqueswing.model.Produto;
+import estoqueswing.model.entidade.Fornecedor;
+
+public class ProdutoFornecedor {
+    private int id;
+    private Fornecedor fornecedor;
+    private Produto produto;
+    double valorProduto;
+
+    public ProdutoFornecedor() {
+    }
+
+    public ProdutoFornecedor(int id, Fornecedor fornecedor, Produto produto, double valorProduto) {
+        this.id = id;
+        this.fornecedor = fornecedor;
+        this.produto = produto;
+        this.valorProduto = valorProduto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(double valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+}
