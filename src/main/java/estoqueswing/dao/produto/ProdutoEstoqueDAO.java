57d9ef2b110ea7170923c48b794b42412abd2d8f
luis10barbo <luis10barbo2020@gmail.com>
2023-11-05 11:30:24 -0300

    feat: Utilizar ProdutoEstoque como parametro para ProdutoEstoqueDAO

--- src/main/java/estoqueswing/dao/produto/ProdutoEstoqueDAO.java
+++ src/main/java/estoqueswing/dao/produto/ProdutoEstoqueDAO.java
@@ -1,6 +1,7 @@
 package estoqueswing.dao.produto;
 
 import estoqueswing.dao.Conexao;
+import estoqueswing.dao.EstoqueDAO;
 import estoqueswing.model.Estoque;
 import estoqueswing.model.produto.Produto;
 import estoqueswing.model.produto.ProdutoEstoque;
@@ -25,45 +26,46 @@ public class ProdutoEstoqueDAO {
             "FOREIGN KEY (idProduto) REFERENCES produtos(idProduto)" +
             ")";
 
-        public static long adicionarProdutoEstoque (Produto produto, Estoque estoque, double valorVenda){
+        public static long adicionarProdutoEstoque (ProdutoEstoque produtoEstoque){
             Connection conexao = Conexao.adquirir();
             try{
                 PreparedStatement stmt = conexao.prepareStatement("INSERT INTO produtosEstoque (idEstoque,idProduto,valorGasto,valorVenda,quantidade) VALUES (?,?,?,?,?)");
-                stmt.setInt(1,estoque.getIdEstoque());
-                stmt.setInt(2,produto.getId());
-                stmt.setDouble(3,produto.getValorProduto()*produto.getQuantidade());
-                stmt.setDouble(4,valorVenda);
-                stmt.setInt(5,estoque.getIdEstoque());
+
+                stmt.setInt(1,produtoEstoque.getEstoque().getIdEstoque());
+                stmt.setInt(2,produtoEstoque.getProduto().getId());
+                stmt.setDouble(3,produtoEstoque.getProduto().getValorProduto() * produtoEstoque.getProduto().getQuantidade());
+                stmt.setDouble(4,produtoEstoque.getValorVenda());
+                stmt.setInt(5,produtoEstoque.getProduto().getQuantidade());
                 stmt.executeUpdate();
 
                 Integer id = UtilsSQLITE.ultimoIDInserido(conexao.createStatement());
+                if (id == null) return 0;
                 return id;
 
             }catch (SQLException e){
                 throw new RuntimeException(e);
             }
         }
-        public static boolean removerProdutoEstoque(Produto produto, Estoque estoque){
+        public static boolean removerProdutoEstoque(ProdutoEstoque produtoEstoque){
             Connection conexao = Conexao.adquirir();
             try{
-                PreparedStatement stmt = conexao.prepareStatement("DELETE produtosEstoque WHERE idProduto = ? AND idEstoque = ?");
-                stmt.setInt(1,produto.getId());
-                stmt.setInt(2,estoque.getIdEstoque());
+                PreparedStatement stmt = conexao.prepareStatement("DELETE produtosEstoque WHERE idProdutoEstoque = ?");
+                stmt.setInt(1,produtoEstoque.getId());
                 return stmt.executeUpdate() > 0;
             }catch (SQLException e){
                 throw new RuntimeException(e);
             }
         }
 
-        public static boolean editarProdutoEstoque(Produto produto, Estoque estoque,double valorGasto, double valorGanho,double valorVenda){
+        public static boolean editarProdutoEstoque(ProdutoEstoque produtoEstoque){
             Connection conexao = Conexao.adquirir();
             try{
                 PreparedStatement stmt = conexao.prepareStatement("UPDATE produtosEstoque SET valorGasto = ?, valorGanho = ?, valorVenda = ?, quantidade = ? WHERE idProduto = ? AND idEstoque = ?");
-                stmt.setDouble(1,valorGasto);
-                stmt.setDouble(2,valorGanho);
-                stmt.setDouble(3,valorVenda);
-                stmt.setInt(4,estoque.getIdEstoque());
-                stmt.setInt(5,produto.getId());
+                stmt.setDouble(1,produtoEstoque.getValorGasto());
+                stmt.setDouble(2,produtoEstoque.getValorGanho());
+                stmt.setDouble(3,produtoEstoque.getValorVenda());
+                stmt.setInt(4,produtoEstoque.getEstoque().getIdEstoque());
+                stmt.setInt(5,produtoEstoque.getProduto().getId());
                 return stmt.executeUpdate() > 0;
 
             }catch (SQLException e){
