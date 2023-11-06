261e1367f566ab81ae735f0b26a534fe01f58978
luis10barbo <luis10barbo2020@gmail.com>
2023-11-06 10:39:24 -0300

    feat: Linkar tabela ordemSaida e ordensEntrada com Ordem

--- src/main/java/estoqueswing/dao/ordem/OrdemEntradaDAO.java
+++ src/main/java/estoqueswing/dao/ordem/OrdemEntradaDAO.java
@@ -3,7 +3,9 @@ package estoqueswing.dao.ordem;
 public class OrdemEntradaDAO {
     public static final String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS ordemEntrada(" +
             "idOrdemEntrada INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "idOrdem INTEGER," +
             "idFornecedor INTEGER," +
-            "FOREIGN KEY (idFornecedor) REFERENCES fornecedores(idFornecedor)" +
+            "FOREIGN KEY (idFornecedor) REFERENCES fornecedores(idFornecedor)," +
+            "FOREIGN KEY (idOrdem) REFERENCES ordens(idOrdem) ON DELETE CASCADE" +
             ")";
 }
--- src/main/java/estoqueswing/dao/ordem/OrdemSaidaDAO.java
+++ src/main/java/estoqueswing/dao/ordem/OrdemSaidaDAO.java
@@ -3,7 +3,10 @@ package estoqueswing.dao.ordem;
 public class OrdemSaidaDAO {
     public static final String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS ordensSaida (" +
             "idOrdemSaida INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "idOrdem INTEGER," +
             "idDestinatario INTEGER," +
-            "FOREIGN KEY (idDestinatario) REFERENCES clientes(idDestinatario)" +
+            "FOREIGN KEY (idDestinatario) REFERENCES clientes(idDestinatario)," +
+            "FOREIGN KEY (idOrdem) REFERENCES ordens(idOrdem) ON DELETE CASCADE" +
+
             ")";
 }
