ce4b4f775bccc6a7a8ea8e3520f36d549f5084be
luis10barbo <luis10barbo2020@gmail.com>
2023-11-08 22:56:26 -0300

    fix(ProdutoOrdemDAO): Adicinadas colunas que faltavam em tabela ProdutoOrdem

--- src/main/java/estoqueswing/dao/produto/ProdutoOrdemDAO.java
+++ src/main/java/estoqueswing/dao/produto/ProdutoOrdemDAO.java
@@ -4,7 +4,10 @@ public class ProdutoOrdemDAO {
     public static final String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS produtoOrdem (" +
             "idProdutoOrdem INTEGER PRIMARY KEY AUTOINCREMENT," +
             "idProduto INTEGER," +
+            "idOrdem INTEGER," +
             "quantidade INTEGER," +
-            "valorProduto REAL" +
+            "valorProduto REAL," +
+            "FOREIGN KEY (idProduto) REFERENCES produtos(idProduto)," +
+            "FOREIGN KEY (idOrdem) REFERENCES ordens(idOrdem) ON DELETE CASCADE" +
             ")";
 }
