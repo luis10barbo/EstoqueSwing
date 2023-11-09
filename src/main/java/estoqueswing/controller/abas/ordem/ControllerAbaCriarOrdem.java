bc6d8099bfa7b0317f7b3c2b2112738c67cf41f9
Vinicius <vlconta.suporte@gmail.com>
2023-11-08 23:39:55 -0300

    Feat(ControllerAbaCriarOrdem e ProdutoOrdemDAO): adicionado criarOrdem,  criar ProdutoOrdemDAO e adquirir ProdutoOrdemDAO

--- src/main/java/estoqueswing/controller/abas/ordem/ControllerAbaCriarOrdem.java
+++ src/main/java/estoqueswing/controller/abas/ordem/ControllerAbaCriarOrdem.java
@@ -1,6 +1,9 @@
 package estoqueswing.controller.abas.ordem;
 
 import estoqueswing.controller.abas.produto.ControllerAbaCriarProduto;
+import estoqueswing.dao.ordem.OrdemDAO;
+import estoqueswing.dao.produto.ProdutoDAO;
+import estoqueswing.dao.produto.ProdutoOrdemDAO;
 import estoqueswing.model.ordem.Ordem;
 import estoqueswing.model.produto.ProdutoOrdem;
 import estoqueswing.view.swing.aba.Aba;
@@ -12,6 +15,13 @@ public class ControllerAbaCriarOrdem {
     }
 
     public void cliqueCriarOrdem(Ordem ordem, ProdutoOrdem[] produtosOrdem) {
+        OrdemDAO.criarOrdem(ordem);
+        for (ProdutoOrdem produtoOrdem:produtosOrdem) {
+            if(produtoOrdem.getProduto().getId()==0){
+                ProdutoDAO.adicionarProduto(produtoOrdem.getProduto());
+            }
+            ProdutoOrdemDAO.criar(produtoOrdem);
+        }
         aba.atualizarPagina();
     }
 
