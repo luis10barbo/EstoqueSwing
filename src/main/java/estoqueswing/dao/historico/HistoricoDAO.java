71dea53f6a74057f9a96ea4ce7513fa5c964f0d3
Vinicius <vlconta.suporte@gmail.com>
2023-11-19 17:06:50 -0300

    Progresso HistoricoDao e HistoricoOrdemDAO

--- src/main/java/estoqueswing/dao/historico/HistoricoDAO.java
+++ src/main/java/estoqueswing/dao/historico/HistoricoDAO.java
@@ -1,8 +1,25 @@
 package estoqueswing.dao.historico;
 
+import estoqueswing.model.historico.Historico;
+import estoqueswing.model.historico.HistoricoOrdem;
+import estoqueswing.model.historico.TipoHistorico;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class HistoricoDAO {
     public static String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS historicos (" +
             "idHistorico INTEGER PRIMARY KEY AUTOINCREMENT," +
             "tipoHistorico VARCHAR(16)" +
             ")";
+
+    public static Historico parseHistorico (ResultSet rs) throws SQLException{
+        Historico historico =null;
+        if (rs.getString("tipoHistorico")==TipoHistorico.Ordem.toString()){
+            historico = HistoricoOrdemDAO.adiquirir(rs.getInt("idHistorico"));
+        }
+        return historico;
+    }
+
+
 }
--- src/main/java/estoqueswing/dao/historico/HistoricoOrdemDAO.java
+++ src/main/java/estoqueswing/dao/historico/HistoricoOrdemDAO.java
@@ -1,5 +1,14 @@
 package estoqueswing.dao.historico;
 
+import estoqueswing.dao.BancoDados;
+import estoqueswing.dao.ordem.OrdemDAO;
+import estoqueswing.model.historico.HistoricoOrdem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class HistoricoOrdemDAO {
     public static final String SQL_CRIACAO = "CREATE TABLE IF NOT EXISTS historicosOrdem (" +
             "idHistoricoOrdem INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -8,4 +17,23 @@ public class HistoricoOrdemDAO {
             "FOREIGN KEY (idHistorico) REFERENCES historicos(idHistorico)," +
             "FOREIGN KEY (idOrdem) REFERENCES ordens(idOrdem)" +
             ")";
+
+    public static HistoricoOrdem adiquirir(int idHistorico){
+        Connection conexao = BancoDados.adquirirConexao();
+        try {
+            PreparedStatement stmt = conexao.prepareStatement("SELECT idHistoricoOrdem,idHistorico,idOrdem FROM hisstoricoOrdem WHERE idHistoricoOrdem = ?");
+            stmt.setInt(1,idHistorico);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){
+                return parseHistoricoOrdem(rs);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    private static HistoricoOrdem parseHistoricoOrdem(ResultSet rs) throws SQLException {
+        HistoricoOrdem historicoOrdem = new HistoricoOrdem(OrdemDAO.adquirirOrdem(rs.getInt("idOrdem")));
+        return historicoOrdem;
+    }
 }
