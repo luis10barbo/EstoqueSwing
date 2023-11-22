faabb7681f9968e1ae37fc1bffc6431305674640
luis10barbo <luis10barbo2020@gmail.com>
2023-11-22 19:45:05 -0300

    fix: Atualizar Produtos estoque com margem de venda em valor venda

--- src/main/java/estoqueswing/controller/abas/ordem/ControllerAbaFinalizarOrdem.java
+++ src/main/java/estoqueswing/controller/abas/ordem/ControllerAbaFinalizarOrdem.java
@@ -1,9 +1,11 @@
 package estoqueswing.controller.abas.ordem;
 
 import estoqueswing.dao.ordem.OrdemDAO;
+import estoqueswing.dao.produto.ProdutoEstoqueDAO;
 import estoqueswing.dao.produto.ProdutoOrdemDAO;
 import estoqueswing.model.ordem.NaturezaOrdem;
 import estoqueswing.model.ordem.Ordem;
+import estoqueswing.model.produto.ProdutoEstoque;
 import estoqueswing.model.produto.ProdutoOrdem;
 import estoqueswing.view.swing.JanelaPrincipal;
 import estoqueswing.view.swing.aba.ordem.AbaFinalizarOrdem;
@@ -16,13 +18,15 @@ public class ControllerAbaFinalizarOrdem {
     }
 
     public void cliqueFinalizarOrdem(int margemLucro) {
+        OrdemDAO.finalizarOrdem(aba.getOrdem());
         if (aba.getOrdem().getNatureza() == NaturezaOrdem.Compra) {
             for (ProdutoOrdem produtoOrdem: aba.getOrdem().getProdutosOrdem()) {
-                produtoOrdem.setValorProduto(produtoOrdem.getValorProduto() * ((margemLucro/100.0) + 1));
-                ProdutoOrdemDAO.editar(produtoOrdem);
+                ProdutoEstoque produtoEstoque = ProdutoEstoqueDAO.adquirir(produtoOrdem.getProduto().getId(), 1);
+
+                produtoEstoque.setValorVenda(produtoOrdem.getValorProduto() * ((margemLucro/100.0) + 1));
+                ProdutoEstoqueDAO.editar(produtoEstoque);
             }
         }
-        OrdemDAO.finalizarOrdem(aba.getOrdem());
         JanelaPrincipal.adquirir().voltarAba();
     }
 }
